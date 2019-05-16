af8133ac01d6f14e78fa0152e76e43bde925e904
TamNguyen1210 <mrnguyentrantam@gmail.com>
2019-05-16 16:04:13 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import Routing from "./Routing";
 import store from "./store";
 import { Provider } from "react-redux";
 import Loader from "react-loader-spinner";
+import { client } from "./apollo";
+import { ApolloProvider } from "react-apollo";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Suspense
-        fallback={
-          <Loader type="Ball-Triangle" color="#00BFFF" height="100" width="100" />
-        }
-      >
-        <div className="App">
-          <Routing />
-        </div>
-      </Suspense>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Suspense
+          fallback={
+            <Loader
+              type="Ball-Triangle"
+              color="#00BFFF"
+              height="100"
+              width="100"
+            />
+          }
+        >
+          <div className="App">
+            <Routing />
+          </div>
+        </Suspense>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
